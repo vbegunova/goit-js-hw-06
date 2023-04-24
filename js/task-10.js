85d7b1372ee21882d51a50f5d3fb380771841277
vbegunova <veronikabegunova42@gmail.com>
2023-04-24 07:46:41 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -9,10 +9,10 @@ const number = document.querySelector("#controls input");
 const createBtn = document.querySelector("button[data-create]");
 const destroyBtn = document.querySelector("button[data-destroy]");
 
-const input = document.querySelector('#controls input');
-const minValue = Number(input.getAttribute('min'));
-const maxValue = Number(input.getAttribute('max'));
-const step = Number(input.getAttribute('step'));
+const input = document.querySelector("#controls input");
+const minValue = Number(input.getAttribute("min"));
+const maxValue = Number(input.getAttribute("max"));
+const step = Number(input.getAttribute("step"));
 
 createBtn.addEventListener("click", function () {
   createBoxes(number.value);
@@ -28,25 +28,28 @@ function createBoxes(amount) {
   // }
 
   if (amount > maxValue || amount < minValue) {
-    alert('Введіть значення від 1 до 100!');
-    input.value = '';
+    alert("Введіть значення від 1 до 100!");
+    input.value = "";
     return 0;
   }
 
   let width = 30;
   let height = 30;
-  let markup = [];
+  // let markup = [];
+  let markup = "";
 
   for (let i = 0; i < amount; i += step) {
     const colorValue = getRandomHexColor();
-    markup.push(`<div style="width: ${width}px; height: ${height}px; background-color: ${colorValue};"></div>`) ;
+    // markup.push(`<div style="width: ${width}px; height: ${height}px; background-color: ${colorValue};"></div>`) ;
+    markup += `<div style="width: ${width}px; height: ${height}px; background-color: ${colorValue};"></div>`;
     width += 10;
     height += 10;
   }
-  container.insertAdjacentHTML("beforeend", markup.join(''));
+  // container.insertAdjacentHTML("beforeend", markup.join(''));
+  container.innerHTML = markup;
 }
 
 function destroyBoxes() {
-  input.value = '';
-  container.innerHTML = '';
+  input.value = "";
+  container.innerHTML = "";
 }
